b628a86fd7f62783d1da7df1bfdf6e62ef3f7f17
SzymonZabrocki <szymonzabrocki98@gmail.com>
2021-01-06 14:24:11 +0100

    Create server

---
+++ app.js
@@ -0,0 +1,36 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+
+const app = express()
+
+mongoose.connect(
+    process.env.DBconnection, {useNewUrlParser: true, useUnifiedTopology: true}
+)
+
+app.use(morgan('combined'))
+
+app.use(bodyParser.json())
+
+const userRoutes = require(".api/routes/users")
+const memeRoutes = requie('./api/routes/memes')
+
+app.use('/memes', memeRoutes)
+app.use('/users', userRoutes)
+
+app.use((req, res, next) => {
+    const error = new Error('Nie znaleziono')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        blad: {
+            wiadomosc: error.message,
+        },
+    })
+})
+
+module.exports = app
